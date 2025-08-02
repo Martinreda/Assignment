d8ab94c646446147e06847c3b89c100d7dcbef0d
martin reda <martinreda999@outlook.com>
2025-08-02 10:40:01 +0300

    Q02

---
+++ Assignment/Interface/BasicAuthenticationService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Interface
+{
+    internal class BasicAuthenticationService:IAuthenticationService
+    {
+        private Dictionary<string, string> users = new Dictionary<string, string>()
+    {
+        { "admin", "1234" },
+        { "user", "abcd" }
+    };
+
+        private Dictionary<string, string> roles = new Dictionary<string, string>()
+    {
+        { "admin", "Admin" },
+        { "user", "Viewer" }
+    };
+
+        public bool AuthenticateUser(string username, string password)
+        {
+            return users.ContainsKey(username) && users[username] == password;
+        }
+
+        public bool AuthorizeUser(string username, string role)
+        {
+            return roles.ContainsKey(username) && roles[username] == role;
+        }
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -7,14 +7,29 @@ namespace Assignment
         static void Main(string[] args)
         {
             #region Q01
-            
-            
-                ICircle myCircle = new Circle(5.0);
-                IRectangle myRectangle = new Rectangle(4.0, 6.0);
 
-                myCircle.DisplayShapeInfo();      
-                myRectangle.DisplayShapeInfo();  
-           
+
+            //ICircle myCircle = new Circle(5.0);
+            //IRectangle myRectangle = new Rectangle(4.0, 6.0);
+
+            //myCircle.DisplayShapeInfo();      
+            //myRectangle.DisplayShapeInfo();  
+
+            #endregion
+
+            #region Q02
+            IAuthenticationService authService = new BasicAuthenticationService();
+
+            string username = "admin";
+            string password = "1234";
+            string role = "Admin";
+
+            bool isAuthenticated = authService.AuthenticateUser(username, password);
+            bool isAuthorized = authService.AuthorizeUser(username, role);
+
+            Console.WriteLine($"Authenticated: {isAuthenticated}"); 
+            Console.WriteLine($"Authorized: {isAuthorized}");       
+
             #endregion
         }
     }
