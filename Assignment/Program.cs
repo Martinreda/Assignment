4cdb400146febe27974ac39bcb51406a98b0f9cb
martin reda <martinreda999@outlook.com>
2025-08-02 10:30:53 +0300

    Q01

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Assignment
+﻿using Assignment.Interface;
+
+namespace Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q01
+            
+            
+                ICircle myCircle = new Circle(5.0);
+                IRectangle myRectangle = new Rectangle(4.0, 6.0);
+
+                myCircle.DisplayShapeInfo();      
+                myRectangle.DisplayShapeInfo();  
+           
+            #endregion
         }
     }
 }
